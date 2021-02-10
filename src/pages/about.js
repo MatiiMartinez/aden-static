6f0f40bbdf5f743d31aaf4ac6565f6451ee5119d
MatiiMartinez <matiase.martinez@aden.org>
2021-02-10 09:54:45 -0300

    .

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import SEO from "../components/seo"
+
+const about = () => {
+  return (
+    <div>
+      <SEO title="Nosotros" description="Descripcion de la pagina" />
+      About
+    </div>
+  )
+}
+
+export default about

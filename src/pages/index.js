160dde72090a83c1534ba6dc415e3122fb5e79f9
MatiiMartinez <matiase.martinez@aden.org>
2021-02-10 10:52:08 -0300

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,35 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Loading from "../components/Loading"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
-  useEffect(() => {
+  /*useEffect(() => {
     const isBrowser = () => typeof window !== "undefined"
     isBrowser() && window.location.replace("https://app.aden.org/")
+  }, [])*/
+  const [item, setItem] = useState(null)
+
+  useEffect(() => {
+    async function getSource() {
+      await fetch(`${process.env.GATSBY_REPO}workshop/5626`)
+        .then(res => res.json())
+        .then(res => setItem(res))
+        .catch(err => console.log(err))
+    }
+    getSource()
   }, [])
+
+  if (!item) {
+    return <Loading />
+  }
 
   return (
     <>
-      <SEO />
+      <SEO
+        title={item.nombre_publicacion}
+        description={item.descripcion}
+        image={item.imagen}
+      />
       <Loading />
     </>
   )
